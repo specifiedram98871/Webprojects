1f6554f21b2f64b1969642730e2c8540ef23bfcc
unknown <ramkm98871@gmail.com>
2023-08-03 17:48:06 +0545

    near

--- port2/script.js
+++ port2/script.js
@@ -1,3 +1,16 @@
+// toogle navbar
+let menuIcon =document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick=()=>{
+   menuIcon.classList.toggle('fa-x');
+   navbar.classList.toggle('active');
+};
+
+
+
+
+
 let sections =document.querySelectorAll('section');
 let navLinks =document.querySelectorAll('header nav a');
 
@@ -15,4 +28,14 @@ window.onscroll=()=>{
              });
         }
     })
+
+    // stivky navbar
+
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky',window.scrollY > 100);
+
+    // remove toggle icon when clicked
+    menuIcon.classList.remove('fa-x');
+    navbar.classList.remove('active');
+
 }
