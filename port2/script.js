38446bd6dec2fb71a0e889f85c88ba297c544ad1
unknown <ramkm98871@gmail.com>
2023-08-05 12:43:45 +0545

    corrected

--- port2/script.js
+++ port2/script.js
@@ -54,7 +54,7 @@ ScrollReveal().reveal('.home-content p,.about-content',{origin:'right'});
 
 // typed js
 const typed =new Typed('.multiple-text',{
-    strings:['FrontEnd Develiper','Reader','Explorer'],
+    strings:['FrontEnd Developer','Reader','Explorer'],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
