66d9fe1ec8c611aa10a1384379b0618cdc78f694
unknown <ramkm98871@gmail.com>
2023-08-05 12:41:31 +0545

    Complete

--- port2/script.js
+++ port2/script.js
@@ -39,3 +39,24 @@ window.onscroll=()=>{
     navbar.classList.remove('active');
 
 }
+// scroll
+ScrollReveal({
+    reset:true,
+    distance:'80px',
+    duration:2000,
+    delay:200
+});
+
+ScrollReveal().reveal('.home-content,.heading',{origin:'top'});
+ScrollReveal().reveal('.home-img,.services-container,.portfolio-box,.contact form',{origin:'bottom'});
+ScrollReveal().reveal('.home-content h1,.about-img',{origin:'left'});
+ScrollReveal().reveal('.home-content p,.about-content',{origin:'right'});
+
+// typed js
+const typed =new Typed('.multiple-text',{
+    strings:['FrontEnd Develiper','Reader','Explorer'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
